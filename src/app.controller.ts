9a394891b05bf2cfb0642c711e08e71c26c9bfab
vtronghieu <vthiu002@gmail.com>
2025-01-20 22:40:42 +0700

    init2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get("hello")
+  hello(req: Request) {
+    const rs = req;
+    console.log(req);
+    return rs as any;
+  }
 
   @Post("test")
   test(req: Request) {
